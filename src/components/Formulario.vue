bf2f3c384794889e3bdb77c662880de87f478219
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-06 11:50:28 -0600

    Primeros pasos con el Formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -4,13 +4,33 @@
 
 <template>
     <div class="md:w-1/2">
-       <h2 class="font-black text-3xl text-center">
+        <h2 class="font-black text-3xl text-center">
             Seguimiento Pacientes
-       </h2>
-       <p class="text-lg mt-5 text-center mb-10">
+        </h2>
+        <p class="text-lg mt-5 text-center mb-10">
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Administralos</span>
-       </p>
+        </p>
+
+        <form
+            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        >
+            <div class="mb-5">
+                <label 
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Mascota
+                </label>
+                <input
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre de la mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                />
+            </div>
+
+        </form>
     </div>
 </template>
 
