9fc3fce38dc0fa67933775d3bbc9a904544742cb
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-06 14:57:55 -0600

    Creando un componente para mostrar las alertas

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -35,7 +35,10 @@
             Añade Pacientes y
             <span class="text-indigo-600 font-bold">Administralos</span>
         </p>
-
+        <Alerta 
+            v-if="alerta.mensaje"
+            :alerta="alerta"
+        />
         <form
             class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
             @submit.prevent="validar"
