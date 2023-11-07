0fa355cecb76e4a553280dd50e7d138a7c1f6d70
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-11-07 10:55:51 -0600

    Mostrando los Pacientes Agregados

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     import { ref, reactive } from 'vue';
     import Header from './components/Header.vue'
     import Formulario from './components/Formulario.vue'
+    import Paciente from './components/Paciente.vue'
 
     const pacientes = ref([])
 
@@ -36,7 +37,14 @@
                 <h3 class="font-black text-3xl text-center">Administra tus Pacientes</h3>
 
                 <div v-if="pacientes.length > 0">
-
+                    <p class="text-lg mt-5 text-center mb-10">
+                        Información de 
+                        <span class="text-indigo-600 font-bold">Pacientes</span>
+                    </p>
+                    <Paciente 
+                        v-for="paciente in pacientes"
+                        :paciente="paciente"
+                    />
                 </div>
                 <p v-else class="mt-20 text-2xl text-center">
                     No Hay Pacientes
